2af423677f42f8b693f1076465e825e1a186ff05
Bryce Marcus <bmarcus@kippnyccp.org>
2015-04-16 08:37:34 -0400

    Update application.js

--- shopping-list/application.js
+++ shopping-list/application.js
@@ -1,10 +1,10 @@
 $(document).ready(function(){
   $('#submit').on('click', function(){
     var entry = $('input').val();
-    $('ul').append("<li>"+entry+"</li>"); //the item will be appended into the ul as a li element.
+    $('ul').append("<li>"+entry+"</li>");
   })
   $('#clear').on('click', function(){
     var entry = $('input').val();
-    $("ul").empty(); //the items will be removed from the ul.
+    $("ul").empty();
   })
 });
