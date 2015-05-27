638eec4c7d87a9e86887f93bac4929ba24c19ed9
Bryce Marcus <bmarcus@kippnyccp.org>
2015-05-27 09:33:00 -0400

    Create sensomaze.py

---
+++ sensomaze.py
@@ -0,0 +1,25 @@
+from gipigo import
+import time
+import math
+
+set_speed(100)
+
+def move_forward(feet):
+  mm = 304.8 * feet
+  steps = mm/11.344
+  enc_tgt(1,1,steps)
+  time.sleep(.1)
+  fwd()
+  time.sleep(4*feet)
+
+def turn_right():
+  enc_tgt(1,0,15)
+  time.sleep(.1)
+  right()
+  time.sleep(2)
+
+def turn_left():
+  enc_tgt(0,1,15)
+  time.sleep(.1)
+  left()
+  time.sleep(2)
